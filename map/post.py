bc72fa80a21a4aa8ce4000f4fcd08b18b19e3e96
kim-misol <wonderfulpine@gmail.com>
2021-02-23 00:27:16 +0900

    사진 추가

--- map/post.py
+++ map/post.py
@@ -58,9 +58,10 @@ def kakao_map():
             let country_flag = geolocation_dict[i]["country_flag"];
         
             // 마커이미지의 주소입니다
-            imgSelect = 'https://raw.githubusercontent.com/kim-misol/map_coffee/master/img/marker_green_round.png'
+            // imgSelect = 'https://raw.githubusercontent.com/kim-misol/map_coffee/master/img/marker_green_round.png'
+            imgSelect = 'https://raw.githubusercontent.com/kim-misol/map_ip/main/map/public/marker.png'
             var imageSrc = imgSelect,
-            imageSize = new kakao.maps.Size(30, 33), // 마커이미지의 크기입니다
+            imageSize = new kakao.maps.Size(20, 30), // 마커이미지의 크기입니다
             imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
         
             var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
